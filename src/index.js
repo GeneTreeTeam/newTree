e7c4a7d064e128232117dd6a7a65e0eb02b4fb08
jvan1997 <jonathanvan.1997@gmail.com>
2018-12-02 17:11:43 -0800

    Added read/write possibility to MyDiagram and fixed small spelling error

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ firebaseApp.auth().onAuthStateChanged(user => {
         console.log('user signed in or up', user);
         browserHistory.push('/app2');
     } else{
-        console.log('user has signed put or still needs to sign in');
+        console.log('user has signed out or still needs to sign in');
         if(window.location.pathname != "/tree")
             browserHistory.replace('/app' );
     }
